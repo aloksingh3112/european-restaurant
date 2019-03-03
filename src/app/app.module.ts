21e23903cd14ec00ab6447fc2004a6a7c2a23102
aloksingh3112 <aloksingh3112@gmail.com>
2019-03-03 12:11:36 +0530

    add header and search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
